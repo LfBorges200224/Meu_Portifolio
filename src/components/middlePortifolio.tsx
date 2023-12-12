5b2c72499b6fb0852ce1c50dd446fd287b73c5c9
Luis Eduardo Fernandes Borges <108821332+PietroNog1@users.noreply.github.com>
2023-12-12 13:08:29 -0300

    feat new att

--- src/components/middlePortifolio.tsx
+++ src/components/middlePortifolio.tsx
@@ -16,8 +16,9 @@ export const MiddlePortifolio = () =>{
                         <img src="https://media.licdn.com/dms/image/D4D03AQEgGIISI5-9Hw/profile-displayphoto-shrink_800_800/0/1699884291700?e=1707350400&v=beta&t=ewGJx8dc8fRDLAHzIgWcoAnMCgsCwphtbVLykRcJutc"/>
                         <Headlinetipograpy>Luis Eduardo Fernandes Borges</Headlinetipograpy>
                     </HeadLineContainer>
-                        <H1tipograpy>I <span>love</span> creating and <span>developing</span> projects</H1tipograpy>
-                        <Headlinetipograpy>Discover here in this environment, created especially for you, all my projects and technologies</Headlinetipograpy>
+                        <H1tipograpy>I <span>full stack developer</span> I create and <span>develop</span> projects</H1tipograpy>
+                        <Headlinetipograpy>Technology enthusiast passionate about programming and web development.
+My curiosity and creativity drive me to seek new challenges and opportunities to improve my knowledge.</Headlinetipograpy>
                     <ContainerButtonGithub>
                         <ButtonMyGithub href="https://github.com/PietroNog1" target="blank">
                             See My Projects
